f8f65e4464c3ad1bb156504f7547099cd35ca450
minjiyun02 <myun@ucdavis.edu>
2024-03-29 22:07:35 -0700

    resume fixed, ev ado;ifh;kla

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all elements with the class 'hidden'
+    var hiddenElements = document.querySelectorAll('.hidden');
+  
+    // Function to check if an element is in the viewport
+    function isInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    // Function to check if elements are in the viewport and fade them in
+    function fadeInElements() {
+      hiddenElements.forEach(function(element) {
+        if (isInViewport(element)) {
+          element.classList.add('fade-in');
+        }
+      });
+    }
+  
+    // Initial check on page load
+    fadeInElements();
+  
+    // Event listener for scroll to check if elements are in viewport and fade them in
+    window.addEventListener('scroll', function() {
+      fadeInElements();
+    });
+
+    function openPDF() {
+      window.open(document.getElementById('resume-link').getAttribute('href'));
+    }
+  });
